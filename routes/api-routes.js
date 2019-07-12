c992cfaacde9031daa058f64d176a12ffaed36a3
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-07-12 15:54:23 -0500

    re-structured folders to use sequelizer instead of ORM

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+//variable to require our models
+var db = require("../models");
+
+module.exports = function(app) {
+
+    //GET route for getting all of the burgers
+    app.get("/api/burgers", function(req, res){
+
+    });
+}
